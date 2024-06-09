560f5c4aea90d39e68f3f2423cf5d3da17395a59
gabriel-limagg santos <gabriellima140806@gmail.com>
2024-06-09 19:56:16 +0000

    estilização do h1 e do p do card

--- src/pages/Home.styled.js
+++ src/pages/Home.styled.js
@@ -4,6 +4,19 @@ export const CardContainer = styled.div`
  background: ${props => props.theme.cardGradient};
  max-width: 41.2rem;
  padding: 3.2rem;
+ h1{
+      font-size: 2.8rem;
+      color: ${props => props.theme.white};
+      margin-bottom: 1.5rem;
+      align-items: center;
+
+    }
+    p{
+      font-size: 1.5rem;
+      line-height: 2.4rem;
+      color: ${props => props.theme.textColor};
+      align-items: center;
+    }
 `
 
 export const IconContainer = styled.div``
--- src/styles/global.js
+++ src/styles/global.js
@@ -16,6 +16,8 @@ export const GlobalStyle = createGlobalStyle`
     display: flex;
     justify-content: center;
     align-items: center;
+    border-radius: 3rem;
+   
  }
 
 `
